2f86ce875cf471e11f62ac6a11866d4dfe3c8084
Govindaraj K <govindaraj.k@randstad.in>
2026-03-28 03:06:05 +0530

    Updates

--- assets/js/booking-DHKokk2j.js
+++
@@ -1,2 +0,0 @@
-function o(o){window.location.href=function(o){return o?`/booking?service=${o}`:"/booking"}(o)}export{o};
-//# sourceMappingURL=booking-DHKokk2j.js.map
---
+++ assets/js/booking-DIwTiw_j.js
@@ -0,0 +1,2 @@
+const o="/cutsandcombporur/";function n(n){window.location.href=function(n){return n?`${o}booking?service=${n}`:`${o}booking`}(n)}export{n as o};
+//# sourceMappingURL=booking-DIwTiw_j.js.map
